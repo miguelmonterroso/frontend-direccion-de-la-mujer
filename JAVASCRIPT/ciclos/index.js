9272243d87c3a383f5f2808c295de844fd5f9246
Miguel Monterroso <miguelmonterroso39@gmail.com>
2025-06-23 18:50:12 -0600

    CICLOS

---
+++ JAVASCRIPT/ciclos/index.js
@@ -0,0 +1,102 @@
+// TODOS LOS CICLOS DEBEN LLEVAR, 
+// UNA VARIABLE DE CONTROL, 
+// UNA CONDICION 
+// Y UN INCREMENTO
+
+// DO WHILE
+
+// let index = 0;
+
+// // PRIMERO EJECUTA EL CODIGO
+// do{
+//     console.log('do while iteracion', index + 1, 'valor index', index) 
+//     index++
+//  // LUEGO EVALUA LA CONDICION
+// } while (index <= 5) //condicion
+
+
+
+// WHILE 
+
+// PRIMERO EVALUA LA CONDICION
+
+// let index = 0;
+
+// while (index <= 10){
+//     console.log('iteracion', index + 1, index)
+//     index++
+// }
+
+
+// for(let index = 0; index <= 20; index++){
+
+//     if(index === 10){
+//         console.log('el index es igual a 10')
+//     }
+//     else if(index === 15){
+//         console.log('el index es igual a 15')
+//     }
+//     else{
+//         console.log('iteracion', index + 1, index)
+//     }
+// }
+
+
+// array = []
+
+
+// let miPrimerArreglo = ['hola', 5, true, 'adios', 10, 20, 30]
+
+//NOS DICE LA CANTIDAD DE ELEMENTOS DENTRO DE EL ARREGLO
+// console.log(miPrimerArreglo.length)
+
+// .pop elimina el ultimo elemento de la lista
+// miPrimerArreglo.pop()
+
+// console.log(miPrimerArreglo);
+
+// .push podemos agregar elementos
+
+// miPrimerArreglo.push('nuevo elemento')
+
+// console.log(miPrimerArreglo);
+
+let emojis = ['🌆', '🌜', '🀄', '🌂', '🎏', '🎅', '🎌', '🥸']
+
+                           // cantidad total de elementos
+// for(let index = 0; index < emojis.length; index++){
+//     console.log('emoji en el index', index + 1 ,emojis[index])
+// }
+
+
+// HAZ UNA APLICACION QUE HAGA UN CONTEO DE 1 A 100 Y QUE VALIDE LO SIGUIENTE
+
+// SI UN NUMERO ES DIVISIBLE DENTRO DE 3 QUE DIGA LA PALABRA FIZZ
+// SI UN NUMERO ES DIVISIBLE DENTRO DE 5 QUE DIGA LA PALABRA BUZZ
+// SI UN NUMERO ES DIVISIBLE DENTRO DE 3 Y DE 5 QUE DIGA LA PALABRA FIZZBUZZ
+// RESULTADO ESPERADO:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+
+// COMO SABER SI EL RESIDUO ES CERO
+// index % 3 === 0 console.log('fizz')
+
+
+
+
+
+
+
